4f7fab5e11893d1ce63941faf7899f2f1c019690
cmkxak <gcm466@naver.com>
2022-11-23 14:37:37 +0900

    [chore] : 불필요한 import 제거

--- src/main/java/com/ll/jpaexercise/service/BookService.java
+++ src/main/java/com/ll/jpaexercise/service/BookService.java
@@ -1,23 +1,19 @@
 package com.ll.jpaexercise.service;
 
-import com.ll.jpaexercise.entity.Author;
 import com.ll.jpaexercise.entity.Book;
 import com.ll.jpaexercise.entity.dto.BookResponse;
-import com.ll.jpaexercise.repository.AuthorRepository;
 import com.ll.jpaexercise.repository.BookRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
 public class BookService {
     private final BookRepository bookRepository;
-    private final AuthorRepository authorRepository;
 
     public List<BookResponse> findBooks(Pageable pageable) {
         List<Book> books = bookRepository.findAll();
